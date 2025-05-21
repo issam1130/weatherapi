d206923149c9bf1b281b4042389ef26cda4742ca
Student <issam.dbiri@dci-student.org>
2025-05-21 16:16:18 +0200

    changes

--- router/weather.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { getTitle, searchByCity } from "../controller/weatherController.js";
-
-const router = Router();
-
-router.get("/", getTitle);
-router.get("/:input", searchByCity);
-
-export default router;
